7f69a9602abd2b3ca06a7c25ed1e44bec0edb2d4
pablohashescobar <nikhilschacko@gmail.com>
2022-11-28 18:39:45 +0530

    feat: add colour on labels

--- plane/db/models/issue.py
+++ plane/db/models/issue.py
@@ -233,6 +233,7 @@ class Label(ProjectBaseModel):
 
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True)
+    colour = models.CharField(max_length=255, blank=True)
 
     class Meta:
         verbose_name = "Label"
